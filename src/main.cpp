794cedd52ceea054621a7130651f13f3e71d1170
slyzOwO <lilithhimes@gmail.com>
2026-02-16 21:13:42 -0600

    Fix typos, expand furniture list & add stats
    Correct variable name typos (furnature->furniture, color->colour), update furniture entries (added arm_chair, dining_chair, egg chair), and adjust loop references accordingly. Add summary output lines that print counts for furniture/wood/colours and compute Total Lines Generated (colour.size() * wood.size() * furniture.size()). Also emit spacing between groups for readability. A compiled binary (src/main.exe) was added to the changeset.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@ using std::cout, std::cin, std::string;
 
 int main() {
 
-  std::vector<string> furnature = {
-    "chair",
+  std::vector<string> furniture = {
+    "arm_chair",
+    "dining_chair",
+    "egg chair",
     "stool",
   };
 
@@ -24,7 +26,7 @@ int main() {
     "warped",
   };
 
-  std::vector<string> color = {
+  std::vector<string> colour = {
     "white",
     "pink",
     "red", 
@@ -43,13 +45,21 @@ int main() {
     "black" 
   };
 
-for(int i = 0; i < furnature.size(); i++){
+  cout << "Number of Furniture : " << furniture.size() << '\n';
+  cout << "Number of Wood : " << wood.size() << '\n';
+  cout << "Number of Colours : " << colour.size() << "\n\n";
+
+  cout << "Total Lines Generated (TLG) : " << (colour.size() * wood.size()) * furniture.size() << "\n\n";
+
+for(int i = 0; i < furniture.size(); i++){
   for(int j = 0; j < wood.size(); j++){
-    for(int k = 0; k < color.size(); k++){
+    for(int k = 0; k < colour.size(); k++){
       //            "block.decorations.white_cushioned_oak_chair" : "White Cushioned Oak Chair",
-      cout << "\"block.decorations." << color[k] << "_cushioned_"<< wood[j] <<"_"<< furnature[i] <<"\" : \"" << color[k] << " Cushioned "<< wood[j] <<" "<< furnature[i] <<"\"," << '\n';
+      cout << "\"block.decorations." << colour[k] << "_cushioned_"<< wood[j] <<"_"<< furniture[i] <<"\" : \"" << colour[k] << " Cushioned "<< wood[j] <<" "<< furniture[i] <<"\"," << '\n';
     }
+  cout << '\n';
   }
+  cout << '\n';
 }
 
 return 0;
